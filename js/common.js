7b44caacebdc08917565a290c01c47e407f649f9
Kornienko Denis <korney197823@gmail.com>
2016-12-12 16:49:12 +0300

    Refactored all version menu and promo blocks

--- js/common.js
+++ js/common.js
@@ -1,8 +1,9 @@
 'use strict';
 
 var button = document.querySelector('.menu__toggle');
-var menu = document.querySelector('.menu');
-
+var menu = document.querySelector('.menu__item');
+//var menu = menu.classList.remove('menu__item--logo');
+console.log(menu);
 button.addEventListener('click', function () {
-  menu.classList.toggle('menu--close');
+  menu.classList.toggle('menu__item--close');
 });
