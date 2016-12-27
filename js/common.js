df28b1159afeafc40297bd0c48a9e4916ca2467c
Kornienko Denis <korney197823@gmail.com>
2016-12-27 14:03:27 +0300

    created modal window

--- js/common.js
+++ js/common.js
@@ -1,9 +1,27 @@
 'use strict';
 
-var button = document.querySelector('.menu__toggle');
+var menuButton = document.querySelector('.menu__toggle');
 var menu = document.querySelector('.menu__item');
 //var menu = menu.classList.remove('menu__item--logo');
 console.log(menu);
-button.addEventListener('click', function () {
+menuButton.addEventListener('click', function () {
   menu.classList.toggle('menu__item--close');
 });
+
+//Modal window
+
+var openModalBtn = document.querySelector('.modal--open');
+var closeModalBtn = document.querySelector('.modal--close');
+var modal = document.querySelector('.modal');
+var modalContent = document.querySelector('.modal__content');
+
+function openModal() {
+  modal.style.display = 'flex';
+}
+
+function closeModal() {
+  modal.style.display = 'none';
+}
+
+openModalBtn.addEventListener('click', openModal);
+closeModalBtn.addEventListener('click', closeModal, false);
